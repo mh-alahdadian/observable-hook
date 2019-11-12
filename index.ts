3c0d1ea699ca4fb32cdc42b74d7bfbc32905c900
Mohamad Hossein Alahdadian <alahdadian@viratech.ir>
2019-11-12 17:03:37 +0330

    handle class decorates mode

--- index.ts
+++ index.ts
@@ -1,23 +1,49 @@
 import { useEffectOnce, useUpdate } from 'react-use';
 import { useCallback } from 'react';
 
-export default function createObservable<T extends object>(fn: (update: VoidFunction) => T) {
+function createUpdater() {
     const listeners = new Set<VoidFunction>();
     const updateComponents = () => listeners.forEach(fn => fn());
-
-    const store = fn(updateComponents);
-
-    function useStore() {
-        const update = useUpdate();
-        useEffectOnce(() => {
-            listeners.add(update);
-
-            return () => {
-                listeners.delete(update);
-            };
-        });
-        return store;
-    }
-
-    return [store, useStore] as const;
+    const updateEffect = (update) => {
+        listeners.add(update);
+        return () => {
+            listeners.delete(update);
+        };
+    };
+    return {updateComponents, updateEffect};
 }
+const updateSymbol = Symbol('update');
+interface Observable {
+    [updateSymbol]: ReturnType<typeof createUpdater>;
+}
+
+//#region function mode
+export function createObservable<T extends object>(fn: (update: VoidFunction) => T) {
+    const updater = createUpdater();
+    const store = fn(updater.updateComponents);
+    store[updateSymbol] = updater;
+    return store as T;
+}
+//#endregion
+
+//#region class mode
+export const observable: ClassDecorator = (constructor) => {
+    return class extends (constructor as any) {
+        [updateSymbol] = createUpdater();
+    } as any;
+};
+
+export const action: MethodDecorator = (target, propertyKey, descriptor) => {
+    const func = target[propertyKey];
+    descriptor.value = function(...args) {
+        func.call(this, ...args);
+        (this as Observable)[updateSymbol].updateComponents();
+    } as any
+};
+
+export function useObserver<T extends object>(object: T) {
+    const update = useUpdate();
+    useEffectOnce(() => (object as Observable)[updateSymbol].updateEffect(update));
+    return object;
+}
+//#endregion
